98b5dcf06caa62e9db28c0a6d7ee5ca4c04702de
kroy <kroy.xc@gmail.com>
2021-12-16 22:19:59 -0800

    initial commit

---
+++ src/components/focus-tree/Focus.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type FocusProps = {
+  name: string;
+};
+
+function Focus(props: FocusProps) {
+  return (
+    <p>National Focus</p>
+  );
+}
+
+export default Focus;
